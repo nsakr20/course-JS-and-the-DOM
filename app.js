49a90aede7f03ea7391b235230f5faaae1666e5f
nsakr20 <74234685+nsakr20@users.noreply.github.com>
2020-11-10 11:03:52 +0200

    Update app.js

---
+++ app.js
@@ -0,0 +1,7 @@
+const mainHeading = document.querySelector('#main-heading');
+const otherHeading = document.querySelector('#other-heading');
+const excitedText = document.createElement('span');
+
+excitedText.textContent = '!!!';
+mainHeading.appendChild(excitedText);
+otherHeading.appendChild(excitedText);
